e88eff0cca3a6aba755982e44636df8d475601b3
Stephen Le <stephen.dt.le@gmail.com>
2023-02-11 19:47:30 -0500

    Removing cart stuff

--- App.js
+++ App.js
@@ -4,9 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ItemsList } from './components/itemsList.js';
 import { ItemDetails } from './components/itemDetails.js';
-import { Cart } from './components/cart.js';
-import { CartIcon } from './components/cartIcon.js';
-import { CartProvider } from './components/cartContext.js';
 const Stack = createNativeStackNavigator();
 function App() {
   return (
@@ -17,19 +14,16 @@ function App() {
           options={({ navigation }) => ({
             title: 'pepepe',
             headerTitleStyle: styles.headerTitle,
-            headerRight: () => <CartIcon navigation={navigation}/>
           })}/>
           <Stack.Screen name='ProductDetails' component={ItemDetails} 
           options={({ navigation }) => ({
             title: 'Product details',
             headerTitleStyle: styles.headerTitle,
-            headerRight: () => <CartIcon navigation={navigation}/>,
           })} />
           <Stack.Screen name='Cart' component={Cart} 
           options={({ navigation }) => ({
             title: 'My cart',
             headerTitleStyle: styles.headerTitle,
-            headerRight: () => <CartIcon navigation={navigation}/>,
           })} />
         </Stack.Navigator>
       </NavigationContainer>
